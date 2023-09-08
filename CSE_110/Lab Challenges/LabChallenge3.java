3178bbf257b932b5f745804a077757141b57fc39
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2023-09-07 21:04:19 -0500

    error on test 2

--- CSE_110/Lab Challenges/LabChallenge3.java
+++ CSE_110/Lab Challenges/LabChallenge3.java
@@ -31,7 +31,7 @@ public class LabChallenge3 {
         double length_feet = miles * 5280;                              // Miles to Feet
         int width_feet = numOfLanes * 12;                               // Lanes width in feet
         int depth_Feet = (int)(Math.ceil(depthOfAsphalt / 12.0));             // Depth in feet
-        double cubic_feet =  length_feet * width_feet * depth_Feet;     // Getting Cubic Feet - Total
+        double cubic_feet =  (length_feet * width_feet) * depth_Feet;     // Getting Cubic Feet - Total
         double pounds = cubic_feet * 145 ;                               // how many pounds we need 
         double tons = pounds / 2000;                                    // Getting pounds to tons
         truckLoads = (int)(Math.ceil(tons / 5));                        // How many Truck-Loads we need
