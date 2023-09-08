7139bbd8e51208ab0e7a3cc9664399ab22c3f149
Bennie Kowalski <1pokealex@gmail.com>
2023-09-08 17:40:49 -0500

    Added File to compare tests with lab 3

--- CSE_110/Lab Challenges/LabChallenge3.java
+++ CSE_110/Lab Challenges/LabChallenge3.java
@@ -1,50 +1,49 @@
 // Lab 03 -  Truckloads of Asphalt
 
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class LabChallenge3 {
     public static void main(String[] args){
-        // Scanner 
-        //Scanner scan = new Scanner(System.in);
-        
-        // declared Variable
-        double miles;
-        int numOfLanes;
-        int depthOfAsphalt;
-        int truckLoads;
-        double cost;
-        
-        // Input 
-        /*System.out.print("Enter length of road in miles    : ");
-        miles = scan.nextDouble();
-        System.out.print("Enter number of lanes            : ");
-        numOfLanes = scan.nextInt();
-        System.out.print("Enter depth of asphalt in inches : ");
-        depthOfAsphalt = scan.nextInt(); */
+    // Below this comment: declare and instantiate a Scanner
+        Scanner scan = new Scanner(System.in);
+      
+    // Below this comment: declare any variables you may need
+        double length = 0.0;
+        int lanes = 0;
+        int depth = 0;
+        int truckLoads = 0;
+        double cost = 0;
+        // Processing set
+        double roadLengthInFeet = 0.0;
+        double roadWidthInFeet = 0.0;
+        double roadDepthInFeet = 0.0;
+        double cubicFeet = 0.0;
+        double poundsOfAsphalt = 0.0;
+        double tonsOfAsphalt = 0.0;
 
-        // Testing Values
-        miles = 1;
-        numOfLanes = 2;
-        depthOfAsphalt = 12;
+    // Below this comment: collect the required inputs
+        System.out.print("Enter length of road in miles     : ");
+        length = scan.nextDouble();
+        System.out.print("Enter number of lanes             : ");
+        lanes = scan.nextInt();
+        System.out.print("Enter depth of asphalt in inches  : ");
+        depth = scan.nextInt();
+      
+    // Below this comment: write Java code to do the computations needed to determine the correct output
+        roadLengthInFeet = length * 5200;
+        roadWidthInFeet = lanes * 12;
+        roadDepthInFeet = depth / 12.0;
 
-        // Processes
-        double length_feet = miles * 5280;                              // Miles to Feet
-        int width_feet = numOfLanes * 12;                               // Lanes width in feet
-        int depth_Feet = (int)(Math.ceil(depthOfAsphalt / 12.0));             // Depth in feet
-        double cubic_feet =  (length_feet * width_feet) * depth_Feet;     // Getting Cubic Feet - Total
-        double pounds = cubic_feet * 145 ;                               // how many pounds we need 
-        double tons = pounds / 2000;                                    // Getting pounds to tons
-        truckLoads = (int)(Math.ceil(tons / 5));                        // How many Truck-Loads we need
-        cost = truckLoads * 150 * 5;
-        // Output
-        System.out.println(length_feet);
-        System.out.println(width_feet);
-        System.out.println(depth_Feet);
-        System.out.println(cubic_feet);
-        System.out.println(pounds);
-        System.out.println(tons);
+        cubicFeet = roadLengthInFeet * roadWidthInFeet * roadDepthInFeet;
+        poundsOfAsphalt = cubicFeet * 145;
+        tonsOfAsphalt = poundsOfAsphalt / 2000;
+        
+        truckLoads = (int)Math.ceil(tonsOfAsphalt / 5);
+        cost = truckLoads * 5 * 150;
+      
+    // Below this comment: output the correct output
         System.out.println("");
-        System.out.println("Truckloads of asphalt needed is : " + truckLoads);
-        System.out.printf("Total cost of asphalt is        : $%.2f", cost);
+        System.out.printf("Truckloads of asphalt needed is  :  %d\n", truckLoads);
+        System.out.printf("Total cost of asphalt is         : $%.2f\n", cost);
     } 
 }
