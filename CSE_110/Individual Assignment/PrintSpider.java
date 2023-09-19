1bc403c1d449077a62b2fb297d849dcdd88b793b
Bennie Kowalski <1pokealex@gmail.com>
2023-09-19 02:42:08 -0500

    Chapter 6 ---DONE , Chapter 7 -- Started

---
+++ CSE_110/Individual Assignment/PrintSpider.java
@@ -0,0 +1,15 @@
+public class PrintSpider {
+   public static void main(String[] args) {
+      // write your code here below
+      String backLegs = "  / _ \\";
+      String butt = "\\_\\(_)/_/";
+      String eyes = " _//\"\\\\_";
+      String frontLegs = "  /   \\";
+      
+      System.out.println(backLegs);
+      System.out.println(butt);
+      System.out.println(eyes);
+      System.out.println(frontLegs);
+      
+   }
+}
