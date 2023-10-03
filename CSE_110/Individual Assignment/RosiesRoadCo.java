1aa974f22427500682efa1236d693c36e75c680b
Bennie Kowalski <1pokealex@gmail.com>
2023-10-03 14:11:27 -0500

    save

---
+++ CSE_110/Individual Assignment/RosiesRoadCo.java
@@ -0,0 +1,18 @@
+
+public class RosiesRoadCo {
+    public static void main(String[] args) {
+       // write code here to call and test your method before you submit for grading
+       
+    }
+    
+    // write your required method here below
+    public static void printHighwayInfo(){
+
+    }
+
+    public static int numberOfStoplights(double miles, int lanes){
+      
+    }
+    
+    
+ }
