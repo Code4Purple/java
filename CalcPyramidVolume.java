9a775c6d6be9d8959c0afc217f612d91213455de
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2024-03-21 04:27:37 -0500

    Bookmark @ 9.5 / 9.17

---
+++ CalcPyramidVolume.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CalcPyramidVolume {
+
+   public static double pyramidVolume(double length, double width, double height){
+      double baseArea = length * width;
+      double oneThird = 1 / 3.0;
+      //System.out.println(oneThird);
+      double volume = (baseArea * height) * oneThird;
+      return volume;
+   } 
+
+   public static void main (String [] args) {
+      Scanner scnr = new Scanner(System.in);
+      double userLength;
+      double userWidth;
+      double userHeight;
+
+      userLength = scnr.nextDouble();
+      userWidth = scnr.nextDouble();
+      userHeight = scnr.nextDouble();
+
+      System.out.println("Volume: " + pyramidVolume(userLength, userWidth, userHeight));
+   }
+}
