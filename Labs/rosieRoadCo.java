b0945585c3fb9b7f448315d2aa0c230436711fc1
Bennie Kowalski <1pokealex@gmail.com>
2024-03-22 20:03:54 -0500

    Bookmark on Lab

---
+++ Labs/rosieRoadCo.java
@@ -0,0 +1,19 @@
+package Labs;
+
+public class rosieRoadCo {
+    public static void main(String[] args) {
+        
+        /*                             Test Cases  
+        System.out.println(numberOfStopLights(1.0,  1));  //should return the value 3
+        System.out.println(numberOfStopLights(1.0,  2));  //should return the value 4
+        System.out.println(numberOfStopLights(2.0,  1));  //should return the value 6
+        System.out.println(numberOfStopLights(2.75,  3)); //should return the value 10
+        */
+    }
+
+    public static int numberOfStopLights(double miles, int lanes){
+        int intersections = (int)(Math.floor(miles));
+        int stopLights = intersections * (2 + lanes);
+        return stopLights;
+    }
+}
