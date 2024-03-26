4e65b688177ffbb405d2169a6d73e0268f5ab69e
Bennie Kowalski <1pokealex@gmail.com>
2024-03-25 22:11:21 -0500

    Part 4 -- DONE

--- Labs/rosieRoadCo.java
+++ Labs/rosieRoadCo.java
@@ -3,30 +3,6 @@ package Labs;
 public class RosieRoadCo {
     public static void main(String[] args) {
         
-                                   //Test Cases  
-        /*System.out.println(numberOfStopLights(1.0,  1));  //should return the value 3
-        System.out.println(numberOfStopLights(1.0,  2));  //should return the value 4
-        System.out.println(numberOfStopLights(2.0,  1));  //should return the value 6
-        System.out.println(numberOfStopLights(2.75,  3)); //should return the value 10 */
-
-        /*int numberOfStopLights = numberOfStoplights(1.0,  1);
-        System.out.println(numberOfStopLights); */
-
-        /*System.out.println(truckloadsOfAsphalt(1.0, 1, 12));     //should return the value 951
-        System.out.println(truckloadsOfAsphalt(1.0, 2, 12));     //should return the value 1901
-        System.out.println(truckloadsOfAsphalt(2.0, 1, 24));     //should return the value 3802
-        System.out.println(truckloadsOfAsphalt(2.75, 3, 10));    //should return the value 6534 */
-
-        /*System.out.println(numberOfPowerPipes(1.0));   // should return the value 264
-        System.out.println(numberOfPowerPipes(1.5));  // should return the value 396
-        System.out.println(numberOfPowerPipes(2.0));  // should return the value 528
-        System.out.println(numberOfPowerPipes(2.75)); // should return the value 726*/
-
-        /*System.out.println(numberOfWaterPipes(1.0)); //should return the value 352
-        System.out.println(numberOfWaterPipes(1.5)); //should return the value 528
-        System.out.println(numberOfWaterPipes(2.0)); //should return the value 704
-        System.out.println(numberOfWaterPipes(2.75)); //should return the value 968 */
-        
     }
 
     public static int numberOfStoplights(double miles, int lanes){
@@ -55,6 +31,40 @@ public class RosieRoadCo {
        int approximateWaterPipes =  (int)Math.ceil(feet / 15);
        return approximateWaterPipes;
     }
+    public static int numberOfCrewMembers(double miles, int lanes, int days){
+        double crew = (50 * miles * lanes) / days;
+        return (int)Math.ceil(crew);
+    }
 }
 
+
+
+//Test Case
+/*System.out.println(numberOfStopLights(1.0,  1));  //should return the value 3
+System.out.println(numberOfStopLights(1.0,  2));  //should return the value 4
+System.out.println(numberOfStopLights(2.0,  1));  //should return the value 6
+System.out.println(numberOfStopLights(2.75,  3)); //should return the value 10 */
+
+/*int numberOfStopLights = numberOfStoplights(1.0,  1);
+System.out.println(numberOfStopLights); */
+
+/*System.out.println(truckloadsOfAsphalt(1.0, 1, 12));     //should return the value 951
+System.out.println(truckloadsOfAsphalt(1.0, 2, 12));     //should return the value 1901
+System.out.println(truckloadsOfAsphalt(2.0, 1, 24));     //should return the value 3802
+System.out.println(truckloadsOfAsphalt(2.75, 3, 10));    //should return the value 6534 */
+
+/*System.out.println(numberOfPowerPipes(1.0));   // should return the value 264
+System.out.println(numberOfPowerPipes(1.5));  // should return the value 396
+System.out.println(numberOfPowerPipes(2.0));  // should return the value 528
+System.out.println(numberOfPowerPipes(2.75)); // should return the value 726*/
+
+/*System.out.println(numberOfWaterPipes(1.0)); //should return the value 352
+System.out.println(numberOfWaterPipes(1.5)); //should return the value 528
+System.out.println(numberOfWaterPipes(2.0)); //should return the value 704
+System.out.println(numberOfWaterPipes(2.75)); //should return the value 968 */
+
+//System.out.println(numberOfCrewMembers(1.0, 1, 12)); // should return the value 5
+//System.out.println(numberOfCrewMembers(1.0, 2, 15)); // should return the value 7
+//System.out.println(numberOfCrewMembers(2.0, 1, 20)); // should return the value 5
+//System.out.println(numberOfCrewMembers(2.75, 3, 10)); // should return the value 42
 
