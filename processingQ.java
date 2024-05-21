ca51b8e3519765147c72b273f6fcea56c0d217dc
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2024-05-21 13:25:51 -0500

    changing Computers

---
+++ processingQ.java
@@ -0,0 +1,13 @@
+public class processingQ {
+    public static void main(String[] args) {
+        int x;
+        int y;
+  
+        x = 7;
+        y = 5;
+        x += (4 + y);
+        y = 9;
+        System.out.println(x);
+        System.out.println(y);
+    }
+ }
