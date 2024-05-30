9d78f36b7c85315fc2c330c7348f48261dbe135c
Bennie Kowalski <1pokealex@gmail.com>
2024-05-30 18:32:01 -0500

    save

--- processingQ.java
+++ processingQ.java
@@ -1,17 +1,13 @@
 import java.util.Scanner;
 public class processingQ {
     public static void main(String[] args) {
-      String name1;
-      String name2;
+      int myNumber;
+      int yourNumber;
+      
+      myNumber = 7;
+      yourNumber = (myNumber == 7) ? 7 : 0;
 
-      name1 = "Layla";
-      name2 = "Lucy";
-
-      System.out.printf("%-8s|\n", name1);
-      System.out.printf("%-8s|\n", name2);
-
-      double value = 46.0;
-      String scientificNotation = String.format("%e", value);
-      System.out.println(scientificNotation + "%");
+      System.out.println(yourNumber);
+        
     }
 }
