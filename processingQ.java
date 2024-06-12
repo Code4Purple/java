127977495e670ed4db469f9d01efb248506e6faf
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2024-06-11 20:02:10 -0500

    save

--- processingQ.java
+++ processingQ.java
@@ -2,21 +2,11 @@ import java.util.Scanner;
 public class processingQ {
     public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
-      int userAge;
-
-      userAge = scnr.nextInt();
-
-      while (userAge < 16 || userAge > 70) {
-         if (userAge < 16) {
-            System.out.println("5% discount");
-         }
-         else {
-            System.out.println("15% discount");
-         }
-         userAge = scnr.nextInt();
+      int i;
+   
+      for (i = 0; i > -3; --i) {
+         System.out.print(i);
       }
-
-      System.out.print("No discount");
         
     }
 }
