482b817dfeed10908fb6b3710c570a0fc87b965c
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2024-06-18 20:24:21 -0500

    Chapter 10 save

--- workStation.java
+++ workStation.java
@@ -1,37 +1,23 @@
-import java.util.Scanner; // import the Scanner class
-import java.util.ArrayList; // import the ArrayList class
+//import java.util.Scanner; // import the Scanner class
+//import java.util.ArrayList; // import the ArrayList class
 
 public class workStation {
-      public static void printSelectedNumbers(int numCount, int number) {
-            //Scanner scnr = new Scanner(System.in);
-            int i;
-         
-            for (i = 0; i < numCount; ++i) {
-               if (number >= 9) {
-                  System.out.println(number);
-               }
-            }
-          }
-          
-         public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
-            ArrayList<Integer> numbers = new ArrayList<Integer>();
-
-            int numCount = 5;
-            
-            for (int i = 0; i < numCount; i++) {
-               System.out.print("Enter a number: ");
-               int number = input.nextInt();
-               numbers.add(number);
-            }
-            
-            for (int i = 0; i < numbers.size(); i++) {
-               printSelectedNumbers(numCount, numbers.get(i));
-            }
-
-
-            
-
+   public static void main (String [] args) {
+      final int NUM_ELEMENTS = 4;
+      int [] userVals = new int[NUM_ELEMENTS];
+      int i;
+   
+      userVals[0] = -2;
+      userVals[1] = 4;
+      userVals[2] = -5;
+      userVals[3] = 8;
+   
+      for (i = 0; i < userVals.length; ++i) {
+         if (userVals[i] < 0) {
+            userVals[i] = -1 * userVals[i];
          }
+         System.out.println(userVals[i]);
       }
+   }
+}
    
