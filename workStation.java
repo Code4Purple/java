661f89cfcca2e3bd4eb80184857e062bbb1d8b29
Bennie Kowalski <1pokealex@gmail.com>
2024-06-19 09:22:00 -0500

    Chapter 10.1 to 10.8 is DONE, GOT TO DO : 10.9 to 10.25

--- workStation.java
+++ workStation.java
@@ -1,23 +1,31 @@
-//import java.util.Scanner; // import the Scanner class
+import java.util.Scanner; // import the Scanner class
 //import java.util.ArrayList; // import the ArrayList class
 
 public class workStation {
    public static void main (String [] args) {
-      final int NUM_ELEMENTS = 4;
-      int [] userVals = new int[NUM_ELEMENTS];
+      Scanner scnr = new Scanner(System.in);
+      int numElems;
+      int[] userPoints;
       int i;
-   
-      userVals[0] = -2;
-      userVals[1] = 4;
-      userVals[2] = -5;
-      userVals[3] = 8;
-   
-      for (i = 0; i < userVals.length; ++i) {
-         if (userVals[i] < 0) {
-            userVals[i] = -1 * userVals[i];
+
+      numElems = scnr.nextInt();
+
+      userPoints = new int[numElems];
+      
+      for(int j = 0; j < numElems; j++){;
+         if(j == numElems - 2){
+            userPoints[j] = 50;
          }
-         System.out.println(userVals[i]);
+         else{
+            userPoints[j] = 0;
+         }
+         
       }
+
+      System.out.print("Updated points: ");
+		for (i = 0; i < userPoints.length; ++i) {
+			System.out.print(userPoints[i] + " ");
+		}
+		System.out.println();
    }
 }
-   
