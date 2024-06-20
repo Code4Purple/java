c0820b02e448dd3768d11a549c0364a378b50416
Bennie Kowalski <1pokealex@gmail.com>
2024-06-20 09:13:15 -0500

    save

--- workStation.java
+++ workStation.java
@@ -2,30 +2,33 @@ import java.util.Scanner; // import the Scanner class
 //import java.util.ArrayList; // import the ArrayList class
 
 public class workStation {
-   public static void main (String [] args) {
-      Scanner scnr = new Scanner(System.in);
-      int numElems;
-      int[] userPoints;
+   public static void updateScoreVals(int[] scoreVals, int updateVal) {
       int i;
 
-      numElems = scnr.nextInt();
+      for (i = 0; i < scoreVals.length; ++i) {
+          scoreVals[i] = scoreVals[i] + updateVal;
+      }
+   }
+   public static void printScoreVals(int[] scoreVals) {
+       int i;
 
-      userPoints = new int[numElems];
-      
-      for(int j = 0; j < numElems; j++){;
-         if(j == numElems - 2){
-            userPoints[j] = 50;
-         }
-         else{
-            userPoints[j] = 0;
-         }
-         
+       for (i = 0; i < scoreVals.length; ++i) {
+           System.out.println(scoreVals[i]);
+       }
+   }
+
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      final int NUM_SCORES = 4;
+      int[] quizScores = new int[NUM_SCORES];
+      int scoreUpdate = 1;
+      int i;
+
+      for (i = 0; i < quizScores.length; ++i) {
+          quizScores[i] = scnr.nextInt();
       }
 
-      System.out.print("Updated points: ");
-		for (i = 0; i < userPoints.length; ++i) {
-			System.out.print(userPoints[i] + " ");
-		}
-		System.out.println();
+      updateScoreVals(quizScores, scoreUpdate);
+      printScoreVals(quizScores);
    }
 }
