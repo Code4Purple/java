5f5e6f54001b98f90bb5fd38623f5179e347cdac
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2024-06-24 19:35:40 -0500

    Lab 08 Part 4 -- done

--- Summer2024/Chapter10/Lab08.java
+++ Summer2024/Chapter10/Lab08.java
@@ -4,12 +4,44 @@ public class Lab08 {
     public static void main(String[] args) {
         // Test Array
         int[] myArray = {1, 22, 333, 400, 5005, 9};
+        System.out.println("Array: {1, 22, 333, 400, 5005, 9}");
         // Call getFirst method
-        int output = getFirst(myArray);
-        System.out.println(output);
+        System.out.println("First element: " + getFirst(myArray));
+        // Call getLast method
+        System.out.println("Last element: " + getLast(myArray));
+        // Call contains method
+        System.out.println("Deos it Contain 400: " + contains(myArray, 400));
+        // GetAll but the first element
+        System.out.print("All but the first element: ");
+        int[] printArray = getAllButFirst(myArray);
+        for(int i = 0; i < printArray.length; i++){
+            System.out.print(printArray[i] + " ");
+        }
      }
      
     public static int getFirst(int [] numbers) {
         return numbers[0];
     }
+
+    public static int getLast(int [] numbers) {
+        return numbers[numbers.length - 1];
+    }
+
+    public static boolean contains(int [] Array, int searchValue) {
+        for (int i = 0; i < Array.length; i++) {
+            if (Array[i] == searchValue) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int[] getAllButFirst(int[] Array){
+        int newArray[] = new int[Array.length - 1];
+        for(int i = 0; i < newArray.length ; i++){
+            // Skips the first element
+            newArray[i] =  Array[i + 1];
+        }
+        return newArray;
+    }    
 }
