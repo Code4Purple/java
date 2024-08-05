c34d7c66861f549553aac7d9932819d610b4d5a5
Bennie Kowalski <1pokealex@gmail.com>
2024-08-05 18:17:43 -0500

    TEST 1 PASSED

--- Summer2024/Chapter13/Individual_Assignments/13.22_BasicBunny/Main.java
+++ Summer2024/Chapter13/Individual_Assignments/13.22_BasicBunny/Main.java
@@ -1,5 +1,11 @@
 //package Chapter13.Individual_Assignments.Basic_Bunny;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        
+    }
+
+    public static Bunny makeBunny(String name, int age){
+        return new Bunny();
+    }
 }
