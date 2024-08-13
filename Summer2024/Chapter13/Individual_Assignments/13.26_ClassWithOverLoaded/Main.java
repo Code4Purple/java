ea70034e00950d8b204dbd2fdd96ceb61a9223a8
Bennie Kowalski <1pokealex@gmail.com>
2024-08-12 22:53:25 -0500

    Cleaning up before final Submitt

--- Summer2024/Chapter13/Individual_Assignments/13.26_ClassWithOverLoaded/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-   public static void main(String[] args) {
-      Bunny myBunny = new Bunny("Flopsy", 2);
-      Bunny yourBunny = new Bunny("Benny", 9);
-      
-      myBunny.print();
-      yourBunny.print();
-   }
-}
--- Summer2024/Chapter14/Individual_Assignment/15.6_ClimateData/README.md
+++
@@ -1,4 +0,0 @@
-
-We will use this command to compile your code :
-
-javac City.java Main.java ClimateZone.java -Xlint:all -encoding utf-8
